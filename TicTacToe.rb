adc90857f415c748e9dc617ca0e23fbd8174df7c
zybouric <hoang_christine@yahoo.fr>
2018-10-16 19:40:09 +0200

    Maj tictactoe

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,6 +1,7 @@
 
-class Boardgeneral
-attr_accessor :a1, :a2, :a3, :b1, :b2 , :b3, :c1, :c2, :c3
+class Board
+
+	attr_accessor :a1, :a2, :a3, :b1, :b2 , :b3, :c1, :c2, :c3
 	def initialize(a1, a2, a3, b1, b2, b3, c1, c2, c3)
 		@a1 = a1
 		@a2 = a2
@@ -11,19 +12,40 @@ attr_accessor :a1, :a2, :a3, :b1, :b2 , :b3, :c1, :c2, :c3
 		@c1 = c1
 		@c2 = c2 
 		@c3 = c3
-	
-	end	
+	end
+
 	def boardgame
-		#board = Array.new(3) { Array.new(3) {'0'}},0
-		#puts board
 		p ["   ", "  1 ", " 2  ", " 3 "].join("|")
 		puts "----|----|----|----"
-		p ["A  ", " #{@a1}  ", " #{@a2}  ", " #{@a3} "].join("|")		
+		p ["A  ", " #{@a1}  ", "  #{@a2} ", " #{@a3}  "].join("|")		
 		puts "----|----|----|----"		
-		p ["B  ", " #{@b1}  ", " #{@b2}  ", " #{@b3} "].join("|")
+		p ["B  ", " #{@b1}  ", " #{@b2}  ", " #{@b3}  "].join("|")
 		puts "----|----|----|----"
-		p ["C  ", " #{@c1}  ", " #{@c2}  ", " #{@c3} "].join("|")
+		p ["C  ", " #{@c1}  ", " #{@c2}  ", " #{@c3}  "].join("|")
+		winner_is
 	end
+
+	def combi_w 
+		[[@a1,@a2,@a3],
+		[@b1,@b2,@b3],
+		[@c1,@c2,@c3],
+		[@a1,@b1,@c1],
+		[@a2,@b2,@c2],
+		[@a3,@b3,@c3],
+		[@a1,@b2,@c3],
+		[@a3,@b2,@c1]]
+	end
+	
+	def winner_is 
+		combi_w.each do |value|
+			if value[0] == "X" && value[1] == "X" && value[2] == "X"
+				puts "Player1 win"
+			elsif value[0] == "O" && value[1] == "O" && value[2] == "O"
+				puts "player2 win"
+
+			end
+	end
+	end 
 
 	def rules_player1
 	puts "Player 1, select a case inside de grid"
@@ -54,9 +76,11 @@ attr_accessor :a1, :a2, :a3, :b1, :b2 , :b3, :c1, :c2, :c3
 			@c3 = "X"
 		end 
 	end 
-		def rules_player2
+
+	def rules_player2
 	puts "Player2, select a case inside de grid"
 	select_case = gets.chomp.downcase 
+
 		if select_case == "a1"
 			@a1 = "O"
 		elsif
@@ -83,11 +107,9 @@ attr_accessor :a1, :a2, :a3, :b1, :b2 , :b3, :c1, :c2, :c3
 			@c3 = "O"
 		end 
 	end	
-	
 end
 
-
-class Playergame 
+class Player
 	def players
 		puts "Welcome to BouzyTicTacToe"
 		puts "player 1 name : "
@@ -97,31 +119,34 @@ class Playergame
 		@play2 = gets.chomp
 	
 		puts @play1 + ":X" + " vs " + @play2+ ":O"
-
-	end 
-
-	#def firstplayer
-		#first = [@play1,@play2] 
-		#firstgo = first.sample
-		#puts firstgo + " plays first"
 		puts " Let's start"
-	#end
+	end 
 end 
 
-def newplay
-		newgame = Boardgeneral.new(" "," "," "," "," "," "," "," "," ")
-		newgame.boardgame
-		newplayer = Playergame.new
+
+class Game
+	def newplay
+#define players
+		newplayer = Player.new
 		newplayer.players
-		#newplayer.firstplayer
 
+#define new game
+		newgame = Board.new(" "," "," "," "," "," "," "," "," ")
+		newgame.boardgame
+#loop 5 fois l'ensemble du code et des mais comme on ne doit lancer que 5,5fois le code on mets le break entre le player 1 et 2 
+		i = 1
+		while i < 6
+		i += 1
+			newgame.rules_player1
+			newgame.boardgame 
+			break if i >= 6
+			newgame.rules_player2
+			newgame.boardgame 
+		end 
 
-		newgame.rules_player1
-		newgame.boardgame
-		newgame.rules_player2
-		newgame.boardgame
-		
+	end
+
+startgame = Game.new
+startgame.newplay
 end 
-
-newplay
 
